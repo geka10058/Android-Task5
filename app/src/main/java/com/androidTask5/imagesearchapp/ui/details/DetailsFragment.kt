977623a177bc513e3d361ea82c943c65491c7805
Nikita Novik <friendoye@gmail.com>
2021-11-21 18:02:58 +0300

    Improve permission granting flow

--- app/src/main/java/com/androidTask5/imagesearchapp/ui/details/DetailsFragment.kt
+++ app/src/main/java/com/androidTask5/imagesearchapp/ui/details/DetailsFragment.kt
@@ -1,44 +1,38 @@
 package com.androidTask5.imagesearchapp.ui.details
 
+import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
 import android.app.DownloadManager
 import android.content.Context
-import android.content.pm.PackageManager
+import android.content.pm.PackageManager.PERMISSION_GRANTED
 import android.graphics.drawable.Drawable
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
-import android.os.Environment
 import android.os.Environment.DIRECTORY_PICTURES
-import android.view.MenuItem
 import android.view.View
-import android.widget.Toast
-import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.navArgs
 import com.androidTask5.imagesearchapp.R
+import com.androidTask5.imagesearchapp.data.CatPhoto
 import com.androidTask5.imagesearchapp.databinding.FragmentDetailsBinding
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.target.Target
-import kotlinx.android.synthetic.main.item_cat_photo.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import kotlin.LazyThreadSafetyMode.NONE
 
 class DetailsFragment : Fragment(R.layout.fragment_details) {
 
     private val args by navArgs<DetailsFragmentArgs>()
+    private val photo: CatPhoto by lazy(NONE) { args.photo }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val binder = FragmentDetailsBinding.bind(view)
 
         binder.apply {
-            val photo = args.photo
-
             Glide.with(this@DetailsFragment)
                 .load(photo.url)
                 .error(R.drawable.ic_error)
@@ -72,38 +66,43 @@ class DetailsFragment : Fragment(R.layout.fragment_details) {
             textViewDescription.text = photo.id
             buttonSave.setText(R.string.save_image)
             buttonSave.setOnClickListener {
-
-                /*viewLifecycleOwner.lifecycleScope.launch(Dispatchers.IO) { gallerySaveImage(photo.url) }
-                    Toast.makeText(
-                        requireContext(),
-                        R.string.image_uploaded,
-                        Toast.LENGTH_LONG
-                    ).show()*/
-
-                if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P || checkWritePermission()) {
-                    viewLifecycleOwner.lifecycleScope.launch(Dispatchers.IO) {
-                        gallerySaveImage(photo.url)
-                    }
+                if (checkWritePermission()) {
+                    gallerySaveImage(photo.url)
                 }
-
             }
         }
     }
 
-    private fun checkWritePermission(): Boolean {
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            WRITE_PERMISSION_REQUEST_CODE -> {
+                if (grantResults.firstOrNull() == PERMISSION_GRANTED) {
+                    gallerySaveImage(photo.url)
+                }
+            }
+            else -> super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        }
+    }
 
-        return when (PackageManager.PERMISSION_GRANTED) {
+    private fun checkWritePermission(): Boolean {
+        return when {
+            Build.VERSION.SDK_INT > Build.VERSION_CODES.P -> true
             ContextCompat.checkSelfPermission(
                 requireContext(),
-                android.Manifest.permission.WRITE_EXTERNAL_STORAGE
-            ) -> {
+                WRITE_EXTERNAL_STORAGE
+            ) == PERMISSION_GRANTED -> {
                 true
             }
+            // Seems like there is no code for showing permission
+            // rationale: https://developer.android.com/training/permissions/requesting#manage-request-code-yourself
             else -> {
-                ActivityCompat.requestPermissions(
-                    requireActivity(),
-                    arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE),
-                    1
+                requestPermissions(
+                    arrayOf(WRITE_EXTERNAL_STORAGE),
+                    WRITE_PERMISSION_REQUEST_CODE
                 )
                 false
             }
@@ -111,7 +110,6 @@ class DetailsFragment : Fragment(R.layout.fragment_details) {
     }
 
     private fun gallerySaveImage(url: String) {
-
         val filename = url.substringAfterLast("/")
         val request = DownloadManager.Request(Uri.parse(url))
             .setTitle(filename)
@@ -119,5 +117,9 @@ class DetailsFragment : Fragment(R.layout.fragment_details) {
             .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
         (requireContext().getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager)
             .enqueue(request)
+    }
+
+    private companion object {
+        const val WRITE_PERMISSION_REQUEST_CODE = 1
     }
 }
